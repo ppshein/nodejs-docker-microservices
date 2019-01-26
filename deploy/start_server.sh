6cbef14ffa9b62e553bce33dc128f0f7d19b30e5
pyaephyoeshein@gmail.com <pyaephyoeshein@gmail.com>
2019-01-26 23:44:07 +0630

    aws code-deploy changed

--- deploy/start_server.sh
+++ deploy/start_server.sh
@@ -5,13 +5,13 @@ echo "Configuration"
 export NODE_ENV=production
 
 BASE_DIR=/home/ec2-user/
-APPLICATION_DIRECTORY=$BASE_DIR/ufinity-test
+APPLICATION_DIRECTORY=$BASE_DIR/ufinity-test/app
 
 echo "cd"
 
 cd "$APPLICATION_DIRECTORY"
 
-sequelize db:create && sequelize db:migrate
+#sequelize db:create && sequelize db:migrate
 
 # actually start the server
 sudo pm2 start $APPLICATION_DIRECTORY/bin/server.js -i 0 --name "ufinity-test"
