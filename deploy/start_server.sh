877f80ca8ceb328638348c45f0864910332f965b
pyaephyoeshein@gmail.com <pyaephyoeshein@gmail.com>
2019-01-26 23:11:20 +0630

    AWS CodeDeploy configured

---
+++ deploy/start_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Configuration"
+
+export NODE_ENV=production
+
+BASE_DIR=/home/ec2-user/
+APPLICATION_DIRECTORY=$BASE_DIR/ufinity-test
+
+echo "cd"
+
+cd "$APPLICATION_DIRECTORY"
+
+sequelize db:create && sequelize db:migrate
+
+# actually start the server
+sudo pm2 start $APPLICATION_DIRECTORY/bin/server.js -i 0 --name "ufinity-test"
